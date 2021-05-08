889e57f2bb7a29676f2ccf73aae1903575eb3e27
glqdlt <dlfdnd0725@gmail.com>
2021-05-08 17:59:25 +0900

    init

---
+++ src/main/java/com/glqdlt/myhome/jilleo/JilleoApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.myhome.jilleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JilleoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JilleoApplication.class, args);
+    }
+
+}
